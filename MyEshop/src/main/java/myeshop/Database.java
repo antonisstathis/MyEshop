b0c28a2e4d7e211d911402535ecdad3aabf252f5
antonisstathis <antonisstathis95@gmail.com>
2023-03-06 17:04:03 +0200

    Add files via upload

---
+++ MyEshop/src/main/java/myeshop/Database.java
@@ -0,0 +1,76 @@
+package myeshop;
+
+import java.sql.*;
+
+public class Database {
+	
+	private String url = "jdbc:mysql://localhost:3306/myeshop";
+    private String username = "";
+    private String password = "";
+    private Connection conn;
+	
+	public Database() {
+		
+	}
+	
+	public void connectToDatabase() {
+
+        try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conn = DriverManager.getConnection(url,username,password);
+        }
+        catch (Exception e) {
+                System.out.println(e.getMessage());
+        }
+
+	}
+	
+	public Boolean insertCustomer(Customer c) throws Exception {
+		
+		String[] ps = null;
+		
+		Statement stm = conn.createStatement();
+		ps = searchEmail(c.getEmail());
+		if(ps == null) {
+			String query = "INSERT INTO Customers(name,surname,tel,city,address,zipcode,email,password) VALUES(" + "'" + 
+						c.getName() + "'" + "," + "'" + c.getSurname() + "'"  + "," + "'" + c.getTel() + "'" + "," + "'" + c.getCity()
+						+ "'"  + "," + "'" + c.getAddress() + "'"  + "," + "'" + c.getZipcode() + "'" + "," + "'" + c.getEmail() + "'"
+						+ "," + "'" + c.getPassword() + "')";
+			//System.out.println(query);
+			stm.executeUpdate(query);
+			
+			return false;
+		}
+		else {
+			System.out.println("Email already exists.");
+			return true;
+		}
+		
+	}
+	
+	public String[] searchEmail(String email) throws Exception {
+		
+		String[] data = new String[3];
+		Statement stm = conn.createStatement();
+		String query = "select * from Customers where email=" + "'" + email + "'";
+		ResultSet result = stm.executeQuery(query);
+		if(result.next()) {
+			data[0] = (String) result.getString("name");
+			data[1] = (String) result.getString("surname");
+			data[2] = (String) result.getString("password");
+			result.close();
+			
+			return data;
+		}
+		else {
+			return null;
+		}
+		
+	}
+	
+	public void closeConnection() throws SQLException {
+		
+		conn.close();
+	}
+
+}
---
+++ MyEshop/src/main/java/myeshop/Model.java
@@ -0,0 +1,86 @@
+package myeshop;
+
+public class Model {
+	
+	private Database database;
+	private String[] data = {null,null,null}; // name,surname,password
+	
+	public Model() {
+		
+		database = new Database();
+	}
+	
+	public String processSignInData(String email,String passwordForm) {
+		
+		database.connectToDatabase();
+		try {
+			data = database.searchEmail(email);
+			if(data != null) {
+				System.out.println("Email found: correct password = " + data[2] + " password given = " + passwordForm);
+			}
+			else {
+				
+				return "Email wasn't found.";
+			}
+			database.closeConnection();
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		if(data != null && passwordForm != null) {
+			if(data[2].equals(passwordForm)) {
+				
+				return "login successful";
+			}
+			else {
+				
+				return "Wrong password.";
+			}
+		}
+		else {
+			if(data != null)
+				return "Wrong password";
+		}
+		
+		return null;
+	}
+	
+	public String processSignUpData(Customer customer) {
+		
+		Boolean checkEmail = false;
+		
+		database.connectToDatabase();
+		try {
+			checkEmail = database.insertCustomer(customer);
+			database.closeConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(checkEmail) {
+			return "Email already given.";
+		}
+		else {
+			return "Your account was successfully created.";
+		}
+		
+	}
+	
+	public String getName() {
+		
+		return data[0];
+	}
+	
+	public String getSurname() {
+		
+		return data[1];
+	}
+	
+	public String getPassword() {
+		
+		return data[3];
+	}
+
+}
